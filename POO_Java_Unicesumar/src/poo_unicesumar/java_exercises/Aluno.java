711fe3d835e784ee3c50af44f9cb188973b59302
João Paulo <joaopaulo.arteagle17@gmail.com>
2023-08-02 22:22:57 -0300

    adicionando dinamismo na criação de alunos

--- POO_Java_Unicesumar/src/poo_unicesumar/java_exercises/Aluno.java
+++ POO_Java_Unicesumar/src/poo_unicesumar/java_exercises/Aluno.java
@@ -1,14 +1,18 @@
 package poo_unicesumar.java_exercises;
 
 public class Aluno extends Pessoa {
+
+	public static void main(String[] args) {
+
+	}
  
 	private int ra;
 
 //==================CONSTRUCTOR=====================
 
 //=================CONSTRUCTOR WITH PARAMETER=========
-	public Aluno(String Nome, int Idade, int ra) {
-		super(Nome, Idade);
+	public Aluno(String nome, int Idade, int ra) {
+		super(nome, Idade);
 		this.ra = ra;
 	}
 	
@@ -20,6 +24,11 @@ public class Aluno extends Pessoa {
 		public void setRa(int ra) {
 			this.ra = ra;
 		}
+		
+		@Override
+		public String getNome() {
+			return "Aluno: " + super.getNome();
+			}
 	//====================Equals and HashCode================	
 
 	//===========ToString================================
--- POO_Java_Unicesumar/src/poo_unicesumar/java_exercises/Executavel.java
+++ POO_Java_Unicesumar/src/poo_unicesumar/java_exercises/Executavel.java
@@ -3,20 +3,33 @@ package poo_unicesumar.java_exercises;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.JOptionPane;
+
 public class Executavel {
 
     public static void main(String[] args) {
      
     	List<Aluno> alunos =  new ArrayList<Aluno>();
     	
-    Aluno Joao = new Aluno("Joao", 18, 123456789);
-    Aluno Henrique = new Aluno("Henrique", 25, 987654321);
-    Aluno Emanuel = new Aluno("Emanuel", 17, 456123789);
-    Aluno Pedro = new Aluno("Pedro", 19,123789456);
-    alunos.add(Joao);
-    alunos.add(Henrique);
-    alunos.add(Emanuel);
-    alunos.add(Pedro);
+    Aluno joao = new Aluno("Joao", 18, 123456789);
+    Aluno henrique = new Aluno("Henrique", 25, 987654321);
+    Aluno emanuel = new Aluno("Emanuel", 17, 456123789);
+    Aluno pedro = new Aluno("Pedro", 19,123789456);
+    Aluno joao1 = new  Aluno("Joao P", 19, 456897132);
+   
+    
+    String nome = JOptionPane.showInputDialog("Qual o nome do aluno?");
+    int idade = Integer.parseInt(JOptionPane.showInputDialog("Qual a Idade do Aluno?"));
+    int ra = Integer.parseInt(JOptionPane.showInputDialog("Qual o RA do Aluno?"));
+    
+    Aluno teste = new Aluno(nome, idade, ra);
+    
+    alunos.add(teste);
+    alunos.add(joao);
+    alunos.add(henrique);
+    alunos.add(emanuel);
+    alunos.add(pedro);
+    alunos.add(joao1);
     
     Professor prof = new Professor("Renato", 40, "123abc");
     
@@ -25,10 +38,11 @@ public class Executavel {
     System.out.println("============================ALUNOS=================================");
     
     for (Aluno aluno : alunos) {
-    	System.out.println("Nome do aluno: " + aluno.getNome() + ", Idade aluno:" +aluno.getIdade() + ", RA: " + aluno.getRa());
-        System.out.println("===================================================================");
+    	System.out.println("Nome do " + aluno.getNome() + ", Idade aluno:" +aluno.getIdade() + ", RA: " + aluno.getRa());
+        System.out.println("-------------------------------------------------------------------");
 	}
      
+    
       
      
     }
