dc8ac2235f9346b4135968050288447c57142463
João Paulo <joaopaulo.arteagle17@gmail.com>
2023-08-08 13:32:15 -0300

    Deixando o código mais dinamico para a adição de alunos

--- POO_Java_Unicesumar/src/poo_unicesumar/java_exercises/Executavel.java
+++ POO_Java_Unicesumar/src/poo_unicesumar/java_exercises/Executavel.java
@@ -10,29 +10,28 @@ public class Executavel {
     public static void main(String[] args) {
      
     	List<Aluno> alunos =  new ArrayList<Aluno>();
+//===========================ALUNOS=========================================================    	
+  
     	
-    Aluno joao = new Aluno("Joao", 18, 123456789);
-    Aluno henrique = new Aluno("Henrique", 25, 987654321);
-    Aluno emanuel = new Aluno("Emanuel", 17, 456123789);
-    Aluno pedro = new Aluno("Pedro", 19,123789456);
-    Aluno joao1 = new  Aluno("Joao P", 19, 456897132);
+   int Qnt = Integer.parseInt(JOptionPane.showInputDialog("Quantos Alunos ?"));
    
-    
+  for(int x = 1; x <= Qnt; x++) { 
+  
     String nome = JOptionPane.showInputDialog("Qual o nome do aluno?");
     int idade = Integer.parseInt(JOptionPane.showInputDialog("Qual a Idade do Aluno?"));
     int ra = Integer.parseInt(JOptionPane.showInputDialog("Qual o RA do Aluno?"));
     
-    Aluno teste = new Aluno(nome, idade, ra);
+    Aluno aluno1 = new Aluno(nome, idade, ra);
     
-    alunos.add(teste);
-    alunos.add(joao);
-    alunos.add(henrique);
-    alunos.add(emanuel);
-    alunos.add(pedro);
-    alunos.add(joao1);
-    
-    Professor prof = new Professor("Renato", 40, "123abc");
-    
+    alunos.add(aluno1);
+  }
+//========================================PROFESSOR=============================================
+	  String nome = JOptionPane.showInputDialog("Qual o nome do Professor?");
+	  int idade = Integer.parseInt(JOptionPane.showInputDialog("Qual a Idade do Professor?"));
+	  String matricula = JOptionPane.showInputDialog("Qual a Matricula do Professor?");
+	  
+    Professor prof = new Professor(nome, idade, matricula);
+ //================================IMPRESSÃO NO CONSOLE=========================================   
     System.out.println("============================PROFESSOR==============================");
     System.out.println("Nome do Professor: " + prof.getNome() + ", Idade Professor:" + prof.getIdade() + ", Matricula: " + prof.getMatricula());
     System.out.println("============================ALUNOS=================================");
